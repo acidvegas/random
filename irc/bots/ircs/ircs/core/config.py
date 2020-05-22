e2fcae32d83023a829b6ab2ae3bfae759b07e359
acidvegas <acid.vegas@acid.vegas>
2020-05-21 21:01:05 -0400

    for fourone

---
+++ irc/bots/ircs/ircs/core/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# IRC Services (IRCS) - Developed by acidvegas in Python (https://acid.vegas/ircs)
+# config.py
+
+# Connection
+server   = 'localhost'
+port     = 6667
+use_ipv6 = False
+use_ssl  = False
+vhost    = None
+password = None
+
+# Identity
+nickname    = 'IRCS'
+username    = 'ircs'
+realname    = 'IRC Services Bot'
+
+# Admin
+admin_host  = 'CHANGEME'
+oper_passwd = 'CHANGEME'
